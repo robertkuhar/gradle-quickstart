b0252df7bfd8ff0d9f4c89d175ac58128944439a
robert.kuhar <robert.kuhar@climate.com>
2015-01-18 13:47:46 -0800

    HeaderDumperServlet explore what happens when one header key appears more than once in a Request

--- src/main/java/org/rekdev/hello/service/HeaderDumperServlet.java
+++ src/main/java/org/rekdev/hello/service/HeaderDumperServlet.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
